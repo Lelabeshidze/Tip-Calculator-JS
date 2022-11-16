3593ab62b34ce8f2fa94b1b666d783c4956057b8
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2022-11-16 20:05:34 +0400

    fixed calculation  issue

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ const amount = document.getElementById("amount");
 const guests = document.getElementById("guest");
 const quality = document.getElementById("quality");
 const tipAmount = document.getElementById("tip-amount");
-
+const tipTotal = document.getElementById("tip-total");
 calculate = () => {
   const tip = ((amount.value * quality.value) / guests.value).toFixed(2);
-  amount.value = "";
-  guests.value = "";
-  quality.value = "";
+
   if (tip === "NaN") {
     tipAmount.innerHTML = "Tip $0 each";
     showTipAmount();
@@ -15,12 +13,29 @@ calculate = () => {
     tipAmount.innerHTML = "Tip $ " + tip + "each";
     showTipAmount();
   }
+  console.log(tip);
 };
 
 showTipAmount = () => {
   let x = tipAmount;
   x.className = "show";
-  setTimeout(function () {
-    x.className = x.className.replace("show", "");
-  }, 3000);
+
+};
+calculateTotal = () => {
+  const tipTotalAmount = amount.value * quality.value;
+
+  if (tipTotalAmount === "NaN") {
+    tipTotal.innerHTML = "Tip $0";
+    showTipTotalAmount();
+  } else {
+    tipTotal.innerHTML = " Total Tip is $ " + tipTotalAmount;
+    showTipTotalAmount();
+  }
+  console.log(tipTotalAmount);
+};
+
+showTipTotalAmount = () => {
+  let x = tipTotal;
+  x.className = "show";
+
 };
